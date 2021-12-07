e0654659ec8f358d4ff0ff1626480e1ab16c4cdb
Fabienne Kleger <klegerf@bzz.ch>
2021-12-07 13:55:15 +0100

    Initial version

---
+++ script/app.js
@@ -0,0 +1,105 @@
+/**
+ * Webapp with Pagination
+ */
+
+/**
+ * Deklaration der Variablen
+ */
+let index;
+let artikel = undefined;
+
+/**
+ * Ausgabe zeigen
+ */
+function showUI(){
+    //Identification
+    let htmlObj = document.getElementById("beschrieb");
+    htmlObj.innerHTML =
+        `Farbe: ${artikel.farbe}<br>`+
+        `Grösse: ${artikel.size}<br>`+
+        `Beschrieb: ${artikel.beschreibung}<br>`+
+        `Preis: ${artikel.preis}`;
+
+    htmlObj = document.getElementById("bild");
+    htmlObj.innerHTML =
+        `<img src=${artikel.bild} alt="Artikelbild" width="100%">`;
+
+    htmlObj = document.getElementById("titel");
+    htmlObj.innerHTML =
+        `${artikel.art}`;
+
+    //Index im Pagination-Element zeigen
+    htmlObj = document.getElementById("showIndex");
+    //clear
+    htmlObj.innerHTML = "";
+    //set
+    htmlObj.innerHTML = index;
+}
+
+
+/**
+ * Nächster Eintrag (Record) zeigen
+ */
+function showNext() {
+    //Index aus dem Browser-Speicher lesen
+    index = localStorage.getItem("index");
+    //Testausgabe auf der Console
+    console.log(index);
+    //Falls der index nicht definiert ist ...
+    if (index == undefined){
+        //... dann setze den index auf 0 (Anfang)
+        index = 0;
+    } else {
+        //... sonst falls der index eins kleiner als das Maximum ist
+        if (sortimentListe.length-1 > index){
+            //... erhöhe den index um 1
+            index++;
+        }
+    }
+    //speichere wieder den index im Browser-Speicher
+    localStorage.setItem("index", index);
+    //hole den nächst höheren Eintrag
+    artikel = sortimentListe[index];
+    //zeige den Eintrag
+    showUI();
+}
+
+/**
+ *  Vorhergehender Eintrag (Record) zeigen
+ */
+function showPrevious() {
+    //Index aus dem Browser-Speicher lesen
+    index = localStorage.getItem("index");
+    //Testausgabe auf der Console
+    console.log(index);
+    //Falls der index nicht definiert ist ...
+    if (index == undefined){
+        //... dann setze den index auf 0 (Anfang)
+        index = 0;
+    } else {
+        //... sonst falls der index grösser als das Minimum ist
+        if (index > 0){
+            //... vermindere den index um 1
+            index--;
+        }
+    }
+    //speichere wieder den index im Browser-Speicher
+    localStorage.setItem("index", index);
+    //hole den nächst höheren Eintrag
+    artikel = sortimentListe[index];
+    //zeige den Eintrag
+    showUI();
+}
+
+//start app
+//Falls der index nicht definiert ist ...
+if (artikel === undefined){
+    //... dann setze den index auf 0 (Anfang)
+    index = 0;
+    //speichere wieder den index im Browser-Speicher
+    localStorage.setItem("index", index);
+    //hole den nächst höheren Eintrag
+    artikel = sortimentListe[index];
+    //zeige den Eintrag
+    showUI();
+}
